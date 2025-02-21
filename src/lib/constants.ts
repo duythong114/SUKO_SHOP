196794a22217495b8d829812dc11d9f72ad8019a
duythong114 <tranthong.facebook@gmail.com>
2025-02-21 15:00:46 +0700

    create layout

---
+++ src/lib/constants.ts
@@ -0,0 +1,8 @@
+const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Suko eCommerce";
+const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || "Spend less, enjoy more.";
+const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "An ecommerce app with Next.js, Tailwind CSS, and TypeScript.";
+
+export { APP_NAME, APP_SLOGAN, APP_DESCRIPTION };
